3a51b527ed3e7cd5361d3e9eca267a33b0e6c7be
Aaron Seilis <aaron.seilis@seilis.ca>
2019-03-12 07:52:32 -0400

    Add command line arguments to Rust implementation
    Add command line arguments to the Rust implementation using clap.

--- rust/src/rpgmap.rs
+++ rust/src/rpgmap.rs
@@ -1,33 +1,61 @@
 // rpgmap main source file
 //
 // Program for making simple RPG maps. This is the Rust language implementation.
-//use std::clone::Clone;
 
 extern crate rpgtools;
 use rpgtools::GridMap;
 
+extern crate clap;
+use clap::{Arg, App};
 
+/// Test whether an input string can be parsed as an int and return a Result
+/// as per clap's argument validation API.
+fn test_conv_to_int(val: String) -> Result<(), String> {
+    let result = val.parse::<usize>();
+    match result {
+        Ok(_) => Ok(()),
+        Err(_) => Err(String::from("The value must be an integer"))
+    }
+}
 
 fn main() {
-    let mut map = GridMap::new(100, 100);
-    //map.generate_cave(4, 51);
-    //map.generate_random_cells(80);
+    let cli = App::new("RPG map generator")
+                      .version("0.1")
+                      .author("Aaron Seilis <aaron.seilis@seilis.ca>")
+                      .about("A simple map generator for role playing games")
+                      .arg(Arg::with_name("width")
+                           .short("x")
+                           .long("width")
+                           .takes_value(true)
+                           .default_value("100")
+                           .value_name("INT")
+                           .validator(test_conv_to_int)
+                           .help("The horizontal width of the map"))
+                      .arg(Arg::with_name("height")
+                           .short("y")
+                           .long("height")
+                           .takes_value(true)
+                           .default_value("100")
+                           .value_name("INT")
+                           .validator(test_conv_to_int)
+                           .help("The vertical height of the map"))
+                      .arg(Arg::with_name("map-style")
+                           .short("s")
+                           .long("style")
+                           .takes_value(true)
+                           .default_value("halls")
+                           .possible_values(&["halls", "cave"])
+                           .help("The style of map to generate"))
+                      .get_matches();
 
+    let style = cli.value_of("map-style").unwrap();
+    let width: usize = cli.value_of("width").unwrap().parse().unwrap();
+    let height: usize = cli.value_of("height").unwrap().parse().unwrap();
 
-    //// Top left
-    //map.place_room((0, 0), (0, 0));
-    //map.place_room((1, 1), (3, 3));
+    let mut map = GridMap::new(width, height);
 
-    //// Bottom left
-    //map.place_room((0, 99), (0, 99));
-    //map.place_room((1, 98), (3, 96));
-
-    //// Bottom
-    //map.place_room((99, 0), (99, 0));
-    //map.place_room((99, 99), (99, 99));
-
-    //map.place_room((47, 47), (53, 53));
-    map.place_entrance((50, 50));
+    map.place_room((width/2-1, height/2-1), (width/2+1, height/2+1));
+    map.place_entrance((width/2, height/2));
 
     for _ in 0..30 {
         map.place_random_room(10, true);
@@ -38,7 +66,7 @@ fn main() {
     let result = map.draw_to_file("example.png", 10);
 
     match result {
-        Ok(_) => println!("Dungeon generated: {}", filename),
+        Ok(_) => println!("Map generated: {}", filename),
         Err(e) => println!("Error: {}", e),
     }
 }
